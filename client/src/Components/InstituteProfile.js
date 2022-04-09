0aef3d2e456a0b621c42002cbca671df334b460f
Shubham Singh <47594292+shubhamrk2@users.noreply.github.com>
2022-04-09 23:41:39 +0530

    changes

--- client/src/Components/InstituteProfile.js
+++ client/src/Components/InstituteProfile.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import InstituteNavbar from './InstituteNavbar'
+import React from 'react';
+import InstituteNavbar from './InstituteNavbar';
+import insLogo from "../assets/images/insLogo.png";
+import insImg from "../assets/images/welcomeimg.jpg";
 function InstituteProfile() {
   return (
-    <><div>
+    <div className='insProfile'>
+    <div>
       <InstituteNavbar/>
       </div>
-      <div style={{ color: 'red' }}>InstituteProfile</div>
-    </>
+      <div className='header'>
+        <img src={insLogo} className='insLogo'/>
+        <div className='headerIns'>
+        <h2>JSS Mahavidyapeeth</h2>
+        <h1 className='nameIns'>JSS Academy of Technical Education, Noida</h1>
+        <h4 className='affiliation'>Approved by All India Council for Technical Education (AICTE), New Delhi.<br></br>
+UG programs are Accredited by National Board of Accreditation (NBA), New Delhi.<br></br>
+Affiliated to Dr APJ Abdul Kalam Technical University, Uttar Pradesh, Lucknow</h4>
+</div>
+      </div>
+      <hr className='horRule'></hr>
+      <div className='body'>
+      <h1>Welcome to JSSATE, Noida</h1>
+      <div className='bodyInner'>
+        <img src={insImg} className='insImg'/>
+        <p className='innerBodyText'>
+        JSS Academy of Technical Education Noida (JSSATEN) is one of the leading Technical Institutions in the National Capital Region in the State of Uttar Pradesh. Established in the year 1998 by JSS Mahavidyapeetha, Noida, the Institution has set bench marks every year, and grown into an Institution of Excellence in Technical Education. Located in the central part of NOIDA, JSSATEN has become a household name for its excellence in Discipline, Teaching, Training and Placement. Today, JSSATEN has total student strength of over 4000, who are mentored by more than 250 Faculty Members. The Campus has finest accommodation for girls and boys.
+        <br/><br/>
+        
+        YOUR FUTURE OF EXCELLENCE BEGINS HERE<br/>
+OUR COMMITMENT TO QUALITY AND EXCELLENCE MEANS THAT WE ONLY HAVE TRAINED STAFF FOR INNOVATIVE TEACHING AND REALTIME LEARNING PROCESS
+        </p>
+      </div>
+      </div>
+      <footer className='insFooter'>
+
+      </footer>
+    </div>
   )
 }
 
