8fc630001f14807b87b9fba646d9b2d6d87c74e2
Shubham Singh <47594292+shubhamrk2@users.noreply.github.com>
2022-04-10 14:52:18 +0530

    rebuild

--- client/src/Components/SignInUp.js
+++ client/src/Components/SignInUp.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import background from "../assets/background.mp4"
+import React from "react";
+import { Link } from "react-router-dom";
 function SignInUp() {
   return (
-    <div className='parent-div-signup'>
-      
-      <video loop autoPlay muted id='myVideo'>
-        <source src={background} type='video/mp4'/>
-        Your browser does not support this video tag.
-        </video>
-        <div className='index-content'>
-    	<Link to="/loginstudent" className="card">
-        <div className="card-image"></div>
-        <div className="card-text">
-            <h1 id='signInUp'>STUDENT</h1>
+    <div className="backgroundImg">
+      <div className="parent-div-signup">
+        <div className="index-content">
+          <Link to="/loginstudent" className="card">
+            <div className="card-image"></div>
+            <div className="card-text">
+              <h1 id="signInUp">STUDENT</h1>
+            </div>
+          </Link>
+          <Link to="/institutelogin" className="card">
+            <div className="card-image2"></div>
+            <div className="card-text">
+              <h1 id="signInUp">INSTITUTE</h1>
+            </div>
+          </Link>
         </div>
-        </Link>
-        <Link to="/institutelogin" className="card">
-        <div className="card-image2"></div>
-        <div className="card-text">
-            <h1 id='signInUp'>INSTITUTE</h1>
-        </div>
-        </Link>
-        
-    </div>
+      </div>
     </div>
   );
 }
